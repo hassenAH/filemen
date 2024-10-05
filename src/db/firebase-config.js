ec4820a840ffc657f2174978c487acdbdc886000
AHMADI Hassen <61461149+hassenAH@users.noreply.github.com>
2024-10-05 19:47:33 +0100

    first  commit

---
+++ src/db/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBkQiuuxtVg-UzNp6DMKIlDWmH1KA9NdYM",
+  authDomain: "filamen-bc6aa.firebaseapp.com",
+  projectId: "filamen-bc6aa",
+  storageBucket: "filamen-bc6aa.appspot.com",
+  messagingSenderId: "911448123912",
+  appId: "1:911448123912:web:2e1928543143109f8e7fbd",
+  measurementId: "G-4JG1B7CSYF"
+};
